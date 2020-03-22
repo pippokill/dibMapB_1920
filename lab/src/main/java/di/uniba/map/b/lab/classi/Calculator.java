5762d67fc44c67b91cfd53ee9dd5ef602cdd36e9
pierpaolo <pierpaolo@pierpaolo-microhal>
2020-03-22 20:19:27 +0100

    - Classi, Interfacce, Classi Astratte

---
+++ lab/src/main/java/di/uniba/map/b/lab/classi/Calculator.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2020 pierpaolo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package di.uniba.map.b.lab.classi;
+
+/**
+ *
+ * @author pierpaolo
+ */
+public class Calculator {
+
+    private double memory;  //memoria della calcolatrice
+
+    public Calculator() {
+    }
+
+    public Calculator(double memory) {
+        this.memory = memory;
+    }
+
+    public double getMemory() {
+        return memory;
+    }
+
+    public void setMemory(double memory) {
+        this.memory = memory;
+    }
+
+    public void resetMemory() {
+        this.setMemory(0);
+    }
+
+    public double sum(double a, double b) {
+        return a + b;
+    }
+
+    public double diff(double a, double b) {
+        return a - b;
+    }
+
+    public double mul(double a, double b) {
+        return a * b;
+    }
+
+    public double div(double a, double b) {
+        return a / b;
+    }
+
+    public double sqrt(double n) {
+        return Math.sqrt(n);
+    }
+}
