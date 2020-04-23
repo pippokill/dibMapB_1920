9b3c699eab8cc22abbc6f5ec696194bad760f9a6
pierpaolo <pierpaolo@pierpaolo-microhal>
2020-04-23 11:29:40 +0200

    - Aggiunto produttore/consumatore

---
+++ lab/src/main/java/di/uniba/map/b/lab/concorrente/TestProduttoreConsumatore.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2020 pierpaolo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package di.uniba.map.b.lab.concorrente;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ *
+ * @author pierpaolo
+ */
+public class TestProduttoreConsumatore {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException, InterruptedException {
+        BlockingQueue<String> queue = new LinkedBlockingQueue<>(100);
+        BufferedWriter writer = new BufferedWriter(new FileWriter("./produttore.txt"));
+        Produttore p = new Produttore(queue, 10000);
+        Consumatore c = new Consumatore(queue, writer);
+        p.start();
+        c.start();
+        p.join();
+        c.join();
+        writer.close();
+    }
+
+}
