69274b657d16769cda74cab4e1e7274b5e45cdde
pierpaolo <pierpaolo@pierpaolo-microhal>
2020-05-18 13:03:49 +0200

    - Update esercizio lambda expression

--- lab/src/main/java/di/uniba/map/b/lab/lambda/esercizi/Esercizio1.java
+++ lab/src/main/java/di/uniba/map/b/lab/lambda/esercizi/Esercizio1.java
@@ -16,9 +16,18 @@
  */
 package di.uniba.map.b.lab.lambda.esercizi;
 
+import di.uniba.map.b.lab.dataset.movielens.Movie;
 import di.uniba.map.b.lab.dataset.movielens.Movielens;
+import di.uniba.map.b.lab.dataset.movielens.Rating;
+import di.uniba.map.b.lab.dataset.movielens.User;
+import di.uniba.map.b.lab.dataset.movielens.User.Gender;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -35,19 +44,60 @@ public class Esercizio1 {
             movielens.load(new File("resources/movielens1M"));
             //Effettuare le seguenti operazioni utilizzando il più possibile gli stream e le espressioni lambda
             //1. raggruppare tutti gli utenti di genere femminile con età maggiore o uguale a 25 anni in base all’occupazione
-            //INSERT CODE HERE
+            Map<Integer, List<User>> es1 = movielens.getUsers().stream()
+                    .filter(u -> u.getGender() == Gender.FEMALE && u.getAge() >= 25)
+                    .collect(Collectors.groupingBy(User::getOccupation));
             //2. raggruppare tutti gli utenti di genere maschile che hanno come occupazione "unemployed" e raggrupparli in base allo zipcode
-            //INSERT CODE HERE
+            Map<String, List<User>> es2 = movielens.getUsers().stream()
+                    .filter(u -> u.getGender() == Gender.MALE && u.getOccupation() == 19)
+                    .collect(Collectors.groupingBy(User::getZipcode));
             //3. raggruppare tutti gli utenti che hanno come occupazione "unemployed" e contare quanti utenti ci sono per ogni classe di età
-            //INSERT CODE HERE
-            //4. contare quanti movie hanno un rating >3
-            //INSERT CODE HERE
+            Map<Integer, Long> es3 = movielens.getUsers().stream()
+                    .filter(u -> u.getOccupation() == 19)
+                    .collect(Collectors.groupingBy(User::getAge, Collectors.counting()));
+            //4. contare quanti rating >3
+            long es4 = movielens.getRatings().stream()
+                    .filter(r -> r.getRating() > 3)
+                    .count();
             //5. calcolare per ogni movie il rating medio dato dagli utenti
-            //INSERT CODE HERE
+            Map<String, Double> es5 = movielens.getRatings().stream()
+                    .collect(Collectors.groupingBy(Rating::getMovieId, Collectors.averagingInt(Rating::getRating)));
+            //5_1. contare quanti movie hanno rating medio >3
+            //???            
             //6. calcolare il rating medio dei movie di genere “Comedy”
-            //INSERT CODE HERE
+            //NB: questa implementazione richiede DUE espressioni lambda
+            Set<String> comedyMovieSet = movielens.getMovies().stream()
+                    .filter(m -> m.getGenres().contains("Comedy"))
+                    .map(m -> m.getMovieID())
+                    .collect(Collectors.toSet());
+            double es6 = movielens.getRatings().stream()
+                    .filter(r -> comedyMovieSet.contains(r.getMovieId()))
+                    .mapToDouble(r -> r.getRating())
+                    .average().getAsDouble();
+            System.out.println(es6);
+            //NB: questa implementazione è più lenta in quanto ogni volta devo cercare nella lista dei movie
+            double es6_1 = movielens.getRatings().stream()
+                    .filter(r -> {
+                        int idx = movielens.getMovies().indexOf(new Movie(r.getMovieId(), ""));
+                        if (idx >= 0 && movielens.getMovies().get(idx).getGenres().contains("Comedy")) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    })
+                    .mapToDouble(r -> r.getRating())
+                    .average().getAsDouble();
+            System.out.println(es6_1);
             //7. calcolare il rating medio raggruppando per genere del movie
-            //INSERT CODE HERE
+            //raggruppo movie per genre
+            Map<String, Set<String>> movieByGenre = movielens.getMovies().stream()
+                    .collect(MovieByGenre::new, MovieByGenre::accept, MovieByGenre::combine).getMap();
+            //raggruppo ratings per movieId
+            Map<String, List<Double>> ratingByMovie = movielens.getRatings().stream()
+                    .collect(Collectors
+                            .groupingBy(Rating::getMovieId, Collectors
+                                    .mapping(r -> new Double(r.getRating()), Collectors.toList())));
+            //???
         } catch (IOException ex) {
             System.err.println(ex);
         }
