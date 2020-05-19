dd803d16449189483914b3409757109030852a43
pierpaolo <pierpaolo@pierpaolo-microhal>
2020-05-19 19:09:05 +0200

    - Fix esercizi lambda expressions

--- lab/src/main/java/di/uniba/map/b/lab/lambda/esercizi/Esercizio1.java
+++ lab/src/main/java/di/uniba/map/b/lab/lambda/esercizi/Esercizio1.java
@@ -24,10 +24,13 @@ import di.uniba.map.b.lab.dataset.movielens.User.Gender;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  *
@@ -63,7 +66,10 @@ public class Esercizio1 {
             Map<String, Double> es5 = movielens.getRatings().stream()
                     .collect(Collectors.groupingBy(Rating::getMovieId, Collectors.averagingInt(Rating::getRating)));
             //5_1. contare quanti movie hanno rating medio >3
-            //???            
+            long count3 = movielens.getRatings().stream()
+                    .collect(Collectors.groupingBy(Rating::getMovieId, Collectors.averagingInt(Rating::getRating)))
+                    .values().stream().filter(v -> v > 3).count();
+            System.out.println(count3);
             //6. calcolare il rating medio dei movie di genere “Comedy”
             //NB: questa implementazione richiede DUE espressioni lambda
             Set<String> comedyMovieSet = movielens.getMovies().stream()
@@ -76,28 +82,37 @@ public class Esercizio1 {
                     .average().getAsDouble();
             System.out.println(es6);
             //NB: questa implementazione è più lenta in quanto ogni volta devo cercare nella lista dei movie
-            double es6_1 = movielens.getRatings().stream()
-                    .filter(r -> {
-                        int idx = movielens.getMovies().indexOf(new Movie(r.getMovieId(), ""));
-                        if (idx >= 0 && movielens.getMovies().get(idx).getGenres().contains("Comedy")) {
-                            return true;
-                        } else {
-                            return false;
-                        }
-                    })
-                    .mapToDouble(r -> r.getRating())
-                    .average().getAsDouble();
-            System.out.println(es6_1);
+            /*double es6_1 = movielens.getRatings().stream()
+            .filter(r -> {
+            int idx = movielens.getMovies().indexOf(new Movie(r.getMovieId(), ""));
+            if (idx >= 0 && movielens.getMovies().get(idx).getGenres().contains("Comedy")) {
+            return true;
+            } else {
+            return false;
+            }
+            })
+            .mapToDouble(r -> r.getRating())
+            .average().getAsDouble();
+            System.out.println(es6_1);*/
             //7. calcolare il rating medio raggruppando per genere del movie
-            //raggruppo movie per genre
-            Map<String, Set<String>> movieByGenre = movielens.getMovies().stream()
-                    .collect(MovieByGenre::new, MovieByGenre::accept, MovieByGenre::combine).getMap();
-            //raggruppo ratings per movieId
-            Map<String, List<Double>> ratingByMovie = movielens.getRatings().stream()
-                    .collect(Collectors
-                            .groupingBy(Rating::getMovieId, Collectors
-                                    .mapping(r -> new Double(r.getRating()), Collectors.toList())));
-            //???
+            //viene riutilizzata la prima soluzione dell'esercizio 6 che viene applicata ad
+            //ogni genere
+            Map<String,Double> gavg=new HashMap();
+            movielens.getMovies().stream()
+                    .flatMap(m -> m.getGenres().stream())
+                    .collect(Collectors.toSet())
+                    .forEach(g -> {
+                        Set<String> movieset = movielens.getMovies().stream()
+                                .filter(m -> m.getGenres().contains(g))
+                                .map(m -> m.getMovieID())
+                                .collect(Collectors.toSet());
+                        double avg = movielens.getRatings().stream()
+                                .filter(r -> movieset.contains(r.getMovieId()))
+                                .mapToDouble(r -> r.getRating())
+                                .average().getAsDouble();
+                        gavg.put(g,avg);
+                    });
+            System.out.println(gavg);
         } catch (IOException ex) {
             System.err.println(ex);
         }
